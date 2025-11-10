f37dcc6152ea591d8471febccaf1a8fe002482df
rakesh <rakesh@gmail.com>
2025-11-10 20:24:01 +0530

    Change

---
+++ 40-databases/variables.tf
@@ -0,0 +1,33 @@
+
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        "frontend",
+        # bastion
+        "bastion",
+        # frontend load balancer
+        "frontend_alb",
+        # Backend ALB
+        "backend_alb"
+    ]
+}
+
+variable "zone_id" {
+    default = "Z077856331Q3XY0BQGCNA"
+}
+
+variable "domain_name" {
+    default = "expense.icu"
+}
---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z077856331Q3XY0BQGCNA"
+}
+
+variable "domain_name" {
+    default = "expense.icu"
+}
